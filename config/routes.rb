b97ce6a0a5c1087f3f5e345ee8fc926dd389dc97
kei198609 <keep_casting.19860910@hotmail.co.jp>
2023-01-07 15:18:08 +0900

    サインアップ後の画面遷移先指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
-  devise_for :users
+  root to: 'tops#index'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+    }
+
+  devise_scope :user do
+    get 'mypages/home' => 'users/registrations#home'
+  end
+
+  def devise_scope(scope)
+    constraint = lambda do |request|
+      request.env["devise.mapping"] = Devise.mappings[scope]
+      true
+    end
+
+    constraints(constraint) do
+      yield
+    end
+  end
 
 end
